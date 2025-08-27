61f35149644675a250e865210122c622464b27df
TotoMC <52181149+TotoMC-13@users.noreply.github.com>
2025-08-27 09:17:19 -0300

    Create Partitions.py

---
+++ Partitions.py
@@ -0,0 +1,21 @@
+from typing import List, Dict, Tuple
+
+class Solution:
+    def __init__(self):
+        self.res: List[int] = []
+
+    def partitionLabels(self, s: str) -> List[int]:
+        ...
+
+    def get_intervals(self, s: str):
+        intervals: Dict[Tuple[int, int]] = {}
+
+        for letter in s:
+            intervals[letter] = (0,0)
+
+        for letter in intervals.keys():
+            first: int = None
+            last: int = None
+
+            for i, l in enumerate(s):
+                ...
